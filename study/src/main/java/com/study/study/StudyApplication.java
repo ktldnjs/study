6802df90b2b584a2cad0350381619b24dd0d3900
siwonkim <ktldnjs0811@naver.com>
2022-10-13 04:05:44 +0900

    메소드 개념

--- study/src/main/java/com/study/study/StudyApplication.java
+++ study/src/main/java/com/study/study/StudyApplication.java
@@ -8,17 +8,25 @@ import java.util.List;
 @SpringBootApplication
 public class StudyApplication {
 
+	// printInfo 가 메소드임.
+	public static void printInfo() {
+		String title = "Spring";
+		String tutor = "김시원";
+		int weeks = 5;
+		float ratings = 5.0f;
+
+		System.out.println("제목: " + title);
+		System.out.println("튜터: " + tutor);
+		System.out.println("주차: " + weeks);
+		System.out.println("별점: " + ratings);
+	}
+
 	public static void main(String[] args) {
-		// 문자열이 많이 들어간 list 생성
-		List<String> myList = new ArrayList<>();
-		String word1 = "spring";
-		String word2 = "react";
-		myList.add(word1);
-		myList.add(word2);
-		System.out.println(myList);
-		System.out.println(myList.get(1));
-		myList.remove(1);
-		System.out.println(myList);
+		printInfo();
+		printInfo();
+		printInfo();
+		printInfo();
+		printInfo();
 	}
 
 }
