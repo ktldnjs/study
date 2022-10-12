2571eb8781976d5eae0036cc0ed53f22d63f9355
siwonkim <ktldnjs0811@naver.com>
2022-10-13 03:51:43 +0900

    배열 List 생성, get, remove

--- study/src/main/java/com/study/study/StudyApplication.java
+++ study/src/main/java/com/study/study/StudyApplication.java
@@ -1,13 +1,24 @@
 package com.study.study;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class StudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudyApplication.class, args);
+		// 문자열이 많이 들어간 list 생성
+		List<String> myList = new ArrayList<>();
+		String word1 = "spring";
+		String word2 = "react";
+		myList.add(word1);
+		myList.add(word2);
+		System.out.println(myList);
+		System.out.println(myList.get(1));
+		myList.remove(1);
+		System.out.println(myList);
 	}
 
 }
