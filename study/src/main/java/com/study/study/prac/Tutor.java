971080c40f9024e14c3ccf5e874182147fcdb820
siwonkim <ktldnjs0811@naver.com>
2022-10-15 19:58:46 +0900

    [과제] Getter 생성.
    기본값 없기때문에 Setter는 아직 생성안함.

--- study/src/main/java/com/study/study/prac/Tutor.java
+++ study/src/main/java/com/study/study/prac/Tutor.java
@@ -3,11 +3,19 @@ package com.study.study.prac;
 
 
 public class Tutor {
-    public String name;
-    public int bio;
+    private String name;
+    private int bio;
 
     public Tutor(String name, int bio) {
         this.name = name;
         this.bio = bio;
+    }
+
+    // Getter
+    public String getName(){
+        return this.name;
+    }
+    public int getBio(){
+        return this.bio;
     }
 }
--- study/src/main/java/com/study/study/prac/TutorApp.java
+++ study/src/main/java/com/study/study/prac/TutorApp.java
@@ -5,7 +5,7 @@ public class TutorApp {
         String name = "김시원";
         int bio = 3;
         Tutor tutor = new Tutor(name, bio); // 이걸 쓰려면 클래스에 생성자를 추가해줘야함!! 원래 기본생성자는 파라미터 없는거니까.
-        System.out.println(tutor.name);
-        System.out.println((tutor.bio));
+        System.out.println(tutor.getName());
+        System.out.println(tutor.getBio());
     }
 }
