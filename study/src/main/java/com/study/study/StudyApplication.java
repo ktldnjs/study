443ca5edf4d74b37fccc860590daf72937688f55
siwonkim <ktldnjs0811@naver.com>
2022-10-15 20:15:01 +0900

    spring 기본 week01 끝

--- study/src/main/java/com/study/study/StudyApplication.java
+++ study/src/main/java/com/study/study/StudyApplication.java
@@ -2,31 +2,9 @@ package com.study.study;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 public class StudyApplication {
+	public static void main(String[] args){
 
-	// printInfo 가 메소드임.
-	public static void printInfo() {
-		String title = "Spring";
-		String tutor = "김시원";
-		int weeks = 5;
-		float ratings = 5.0f;
-
-		System.out.println("제목: " + title);
-		System.out.println("튜터: " + tutor);
-		System.out.println("주차: " + weeks);
-		System.out.println("별점: " + ratings);
 	}
-
-	public static void main(String[] args) {
-		printInfo();
-		printInfo();
-		printInfo();
-		printInfo();
-		printInfo();
-	}
-
 }
