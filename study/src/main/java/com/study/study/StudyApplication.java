8dd4a72fe5ad1ea5e33fe5136270ed2686ea4973
siwonkim <ktldnjs0811@naver.com>
2022-10-17 01:39:51 +0900

    Timestamped

--- study/src/main/java/com/study/study/StudyApplication.java
+++ study/src/main/java/com/study/study/StudyApplication.java
@@ -1,7 +1,9 @@
 package com.study.study;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class StudyApplication {
 	public static void main(String[] args){
